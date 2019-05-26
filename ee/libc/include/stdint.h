5a647a4d94ca090e86adb606484cc1d12802c11f
Wild Mouse <3snowp7im@gmail.com>
2019-05-26 06:41:21 -0700

    Add stdint

---
+++ ee/libc/include/stdint.h
@@ -0,0 +1,28 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+*/
+
+/**
+ * @file
+ * Definition file for our stdint.
+ */
+
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int int32_t;
+typedef unsigned uint32_t;
+typedef long int64_t;
+typedef unsigned long uint64_t;
+
+#endif /* __STDINT_H__ */
